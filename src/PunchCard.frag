54bcfcb294fd684e29caf86479925d070d401d6b
qyj <qyjbeijing@gmail.com>
2023-03-16 10:47:09 +0800

    MOD: 将整个工程转到vite

--- src/PunchCard.frag
+++ src/PunchCard.frag
@@ -76,5 +76,5 @@ void run(sampler2D code) {
 }
 
 void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = vec4(0.18, 0.45, 0.96, 1.0);
 }
---
+++ src/create-card.ts
@@ -0,0 +1,33 @@
+import { Buffer } from 'buffer'
+
+export const createCard= ()=>{
+    const width = 4;
+    const height = 4;
+    let buffer = Buffer.alloc(width * height * 4, 0);
+    buffer.writeFloatLE(width, 0);
+    buffer.writeFloatLE(height, 4);
+    let index = 8;
+    // punch card code
+    buffer.writeFloatLE(-1, index);         // mov
+    index += 4;
+    buffer.writeFloatLE(10, index);         // vec0    
+    index += 4;
+    buffer.writeFloatLE(1, index);          // 1
+    index += 4;
+    buffer.writeFloatLE(0, index);          // 0
+    index += 4;
+    buffer.writeFloatLE(0, index);          // 0
+    index += 4;
+    buffer.writeFloatLE(1, index);          // 1
+    index += 4;
+    buffer.writeFloatLE(5, index);          // out
+    index += 4;
+    buffer.writeFloatLE(10, index);         // vec0
+    index += 4;
+    buffer.writeFloatLE(0, index);          // 0
+    return {
+        buffer,
+        width,
+        height,
+    }
+}
